615628f74a533ac5a45b1610144469301e19df70
samebere93 <anyanwuse@gmail.com>
2024-06-22 12:21:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is a printf function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

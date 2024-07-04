72619714b169abe3d0d3992b7ac0c32b1881afa2
samebere93 <anyanwuse@gmail.com>
2024-07-04 11:32:19 -0700

    lets compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,9 @@
 
 /**
  * _strcmp - function that compares two strings
- * @s1: value 1
- * @s2: value 2
- * Return: a string
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: a int
  */
 
 int _strcmp(char *s1, char *s2)
@@ -13,16 +13,13 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0 ; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-	if (s1[i] != s2[i])
-	{
-	if (s1[i] < s2[i])
-		return (s1[i] - s2[i]);
-
-		else if (s2[i] < s1[i])
-			return (s2[i] - s1[i]);
-	}
-
-
+		if (s1[i] != s2[i])
+		{
+			if (s1[i] < s2[i])
+				return (s1[i] - s2[i]);
+			else if (s2[i] < s1[i])
+				return (s1[i] - s2[i]);
+		}
 	}
 	return (0);
 }

2672f5dbfa84fea0c3489991a91fc3d1583288f5
samebere93 <anyanwuse@gmail.com>
2024-07-23 11:30:09 -0700

    Defining Dog structure

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+readme file of 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A program structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

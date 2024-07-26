d8b200bae48168af34a53b8b0403af4a137d4bea
samebere93 <anyanwuse@gmail.com>
2024-07-25 21:17:59 -0700

    print anything provided

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - print any argument provided to the function
+ * @format: the argument specifier
+ * Return: any argument given based on format specifier
+ */
+
+void print_all(const char * const format, ...)
+{
+	int k, check_stat;
+
+	char *str;
+	va_list spc;
+
+	va_start(spc, format);
+
+	k = 0;
+	while (format != NULL && format[k] != '\0')
+	{
+		switch (format[k])
+		{
+			case 'i':
+				printf("%d", va_arg(spc, int));
+				check_stat = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(spc, double));
+				check_stat = 0;
+				break;
+			case 'c':
+				printf("%c", va_arg(spc, int));
+				check_stat = 0;
+				break;
+			case 's':
+				str = va_arg(spc, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				check_stat = 1;
+				break;
+		}
+		if (format[k + 1] != '\0' && check_stat == 0)
+			printf(", ");
+		k++;
+	}
+	printf("\n");
+	va_end(spc);
+}

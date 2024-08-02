fdc4df3c4cd7e0c09fe72970556b52a110a289f2
samebere93 <anyanwuse@gmail.com>
2024-08-02 14:16:13 -0700

    the hair and the tortoise function before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void ComeFirstbySAM(void)__attribute__ ((constructor));
+/**
+ * ComeFirst - function that runs before the main function
+ */
+
+void ComeFirstbySAM(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

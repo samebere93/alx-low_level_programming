1d86a480ffd912a3164ff21f6c33c7ef4c604c1f
samebere93 <anyanwuse@gmail.com>
2024-08-05 13:17:10 -0700

    printing all int element of a single linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all elements in a single linked list
+ * @h: the head pointer to the list
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0 ; h != NULL ; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+readme file for 0x13-more_singly_linked_lists

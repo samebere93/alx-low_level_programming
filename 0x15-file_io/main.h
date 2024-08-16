025a4c81a1b7accf9525f067c0d31843a9a88d5e
samebere93 <anyanwuse@gmail.com>
2024-08-16 15:34:57 -0700

    updated copying a content of a file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,7 +11,5 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void error_file(int file_from, int file_to, char *argv[]);
-int main(int argc, char *argv[]);
 
 #endif
